128b5fcb9e5073dee411a2bcc62b4bed11f1e020
ChrolloRifat <officialmhrifat@outlook.com>
2025-05-10 22:45:44 +0600

    Edit Screenshot script

--- modules/desktop/hyprland/scripts/screenshot.sh
+++ modules/desktop/hyprland/scripts/screenshot.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
+swpy_dir="${XDG_CONFIG_HOME:-$HOME/.config}/swappy"
 XDG_PICTURES_DIR="${XDG_PICTURES_DIR:-$HOME/Pictures}"
 save_dir="${XDG_PICTURES_DIR}/Screenshots"
 save_file="$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')"
 
-# Ensure required binaries
-command -v grimblast >/dev/null 2>&1 || { echo "grimblast not found"; exit 1; }
-command -v swappy >/dev/null 2>&1 || { echo "swappy not found"; exit 1; }
-command -v notify-send >/dev/null 2>&1 || { echo "notify-send not found"; exit 1; }
+mkdir -p $save_dir
+mkdir -p $swpy_dir
+echo -e "[Default]\nsave_dir=$save_dir\nsave_filename_format=$save_file" > $swpy_dir/config
 
 print_error() {
     cat << EOF
